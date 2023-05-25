56a8ee115673640e68c83da9cb29f8d449107113
lls <1165019260@qq.com>
2023-05-25 22:44:02 +0800

    生成测试报告

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -1,16 +1,15 @@
 import time
 import unittest
-
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-
-from utils.config import Config, DATA_PATH
+from utils.config import Config, DATA_PATH, DRIVER_PATH, REPORT_PATH
 from utils.file_reader import ExcelReader
 from utils.log import logger
+from utils.HTMLTestRunner_PY3.HTMLTestRunner_PY3 import HTMLTestRunner
 
 
-class TestBaiDui(unittest.TestCase):
+class TestBaiDu(unittest.TestCase):
     URL = Config().get('URL')
     excel = DATA_PATH + '/baidu.xlsx'
 
@@ -18,27 +17,11 @@ class TestBaiDui(unittest.TestCase):
     locator_result = (By.XPATH, "//h3[@class='LC20lb MBeuO DKV0Md']")
 
     def sub_setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
         self.driver.get(self.URL)
 
     def sub_tearDown(self):
         self.driver.quit()
-
-    # def test_search_0(self):
-    #     self.driver.find_element(*self.locator_kw).send_keys('selenium 灰蓝')
-    #     self.driver.find_element(*self.locator_kw).send_keys(Keys.ENTER)
-    #     time.sleep(2)
-    #     links = self.driver.find_elements(*self.locator_result)
-    #     for link in links:
-    #         logger.info(link.text)
-    #
-    # def test_search_1(self):
-    #     self.driver.find_element(*self.locator_kw).send_keys('python')
-    #     self.driver.find_element(*self.locator_kw).send_keys(Keys.ENTER)
-    #     time.sleep(2)
-    #     links = self.driver.find_elements(*self.locator_result)
-    #     for link in links:
-    #         logger.info(link.text)
 
     def test_search(self):
         datas = ExcelReader(self.excel).data
@@ -55,4 +38,7 @@ class TestBaiDui(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    report = REPORT_PATH + 'case.html'
+    with open(report, 'wb') as f:
+        runner = HTMLTestRunner(f, verbosity=2, title='从0搭建测试框架 灰蓝', description='修改html报告')
+        runner.run(TestBaiDu('test_search'))
--- utils/config.py
+++ utils/config.py
@@ -25,3 +25,7 @@ class Config:
         这样我们其实可以把框架相关的配置放在默认节，其他的关于项目的配置放在其他节中。可以在框架中实现多个项目的测试。
         """
         return self.config[index].get(element)
+
+if __name__ == '__main__':
+    c = Config()
+    print(c.get('URL'))
