25608ec997ad5c067467a51d64df49da4e4e4625
lls <1165019260@qq.com>
2023-05-31 19:53:41 +0800

    http请求封装

---
+++ utils/client.py
@@ -0,0 +1,49 @@
+import requests
+from utils.log import logger
+import json
+
+METHODS = ['GET', 'POST', 'HEAD', 'TRACE', 'PUT', 'DELETE', 'OPTIONS', 'CONNECT']
+
+class UnSupportMethodException(Exception):
+    """当传入的method的参数不是支持的类型时抛出此异常。"""
+    pass
+
+class HTTPClient(object):
+
+    def __init__(self, url, method, headers=None, cookies=None):
+        """headers: 字典。 例：headers={'Content_Type':'text/html'}，cookies也是字典。"""
+        self.url = url
+        self.session = requests.session()
+        self.method = method.upper()
+        if self.method not in METHODS:
+            raise UnSupportMethodException('不支持的method:{0}，请检查传入参数！'.format(self.method))
+
+        self.set_headers(headers)
+        self.set_cookies(cookies)
+
+    def set_headers(self, headers):
+        if headers:
+            self.session.headers.update(headers)
+
+    def set_cookies(self, cookies):
+        if cookies:
+            self.session.cookies.update(cookies)
+
+    def send(self, json=None, data=None):
+        if type(data) == str:
+            data = eval(data)
+
+        if self.method == 'GET':
+            response = self.session.request(method=self.method, url=self.url, params=data)
+        elif self.method == 'POST':
+            if json:
+                response = self.session.request(method=self.method, url=self.url, json=json)
+            else:
+                response = self.session.request(method=self.method, url=self.url, data=data)
+        else:
+            response = None
+
+        response.encoding = 'utf-8'
+        logger.debug('{0} {1}'.format(self.method, self.url))
+        logger.debug('请求成功: {0}\n{1}'.format(response, response.text))
+        return response
