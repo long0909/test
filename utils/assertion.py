8a66a695394695aae9778ff734223c31bcfffb5e
lls <1165019260@qq.com>
2023-05-31 19:52:54 +0800

    断言

---
+++ utils/assertion.py
@@ -0,0 +1,8 @@
+# 判断http的响应码和预期状态码是否一致
+def assertHTTPCode(response, code_list=None):
+    code_list = [1100, 1902]
+    res_code = response.status_code
+    if not code_list:
+        code_list = [200]
+    if res_code not in code_list:
+        raise AssertionError('响应code不在列表中！')  # 抛出AssertionError，unittest会自动判别为用例Failure，不是Error
